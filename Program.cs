88debf03ca3a26cbd66b6362f2f306fd5736b699
Mara Hart <maralihart@gmail.com>
2021-03-27 00:41:33 -0400

    set up csharp hello name

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace LearnCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // STEP 1:
+            // Console.WriteLine("Hello World!");
+
+            // STEP 2:
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {date:d} at {date:t}!"); // The $ lets you use variable names in {}, similar to JavaScript!
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true); // This leaves it open until a key is pressed
         }
     }
 }
